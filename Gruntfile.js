223c3af46fdfbb57d4aae77f0499c5b64f289fc8
vwolfley <vern.wolfley@gmail.com>
2014-03-19 12:12:17 -0700

    reformated grund init Config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,26 +1,38 @@
 module.exports = function(grunt){
 
+    "use strict";
+
     require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
     grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
+
+        pkg: grunt.file.readJSON('package.json'),
 
         htmlhint: {
             build: {
                 options: {
                     'tag-pair': true,
+                    // Force tags to have a closing pair
                     'tagname-lowercase': true,
+                    // Force tags to be lowercase
                     'attr-lowercase': true,
+                    // Force attribute names to be lowercase e.g. <div id="header"> is invalid
                     'attr-value-double-quotes': true,
+                    // Force attributes to have double quotes rather than single
                     'doctype-first': true,
+                    // Force the DOCTYPE declaration to come first in the document
                     'spec-char-escape': true,
+                    // Force special characters to be escaped
                     'id-unique': true,
-                    'head-script-disabled': true,
+                    // Prevent using the same ID multiple times in a document
+                    'head-script-disabled': false,
+                    // Prevent script tags being loaded in the  for performance reasons
                     'style-disabled': true
+                    // Prevent style tags. CSS should be loaded through
                 },
                 src: ['index.html']
             }
-        }
+        },
 
         watch: {
             html: {
