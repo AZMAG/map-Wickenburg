2ba1960125fd30a4e4ad5a9be4ade122b429596b
vwolfley <vern.wolfley@gmail.com>
2014-03-19 16:04:12 -0700

    Add JSHint to Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -30,14 +30,54 @@ module.exports = function(grunt){
                     'style-disabled': true
                     // Prevent style tags. CSS should be loaded through
                 },
-                src: ['index.html']
+                src: ['index.html', 'app/views/*html']
             }
         },
 
+        // CSSLint. Tests CSS code quality
+        // https://github.com/gruntjs/grunt-contrib-csslint
+        csslint: {
+            // define the files to lint
+            files: ['app/resources/css/main.css'],
+                strict: {
+                    options: {
+                        "import": 0,
+                        "empty-rules": 0,
+                        "display-property-grouping": 0,
+                        "shorthand": 0,
+                        "font-sizes": 0,
+                        "zero-units": 0,
+                        "important": 0,
+                        "duplicate-properties": 0,
+                    }
+            }
+        },
+
+        jshint: {
+            all: ['app/model/main.js', 'Gruntfile.js'],
+                options: {
+                    "strict": true,
+                    "sub": true,
+                    "quotmark": "double",
+                    "trailing": true,
+                    "dojo": true,
+                    "jquery": true
+                }
+        },
+
+
         watch: {
             html: {
-                files: ['index.html'],
+                files: ['index.html', 'app/views/*html'],
                 tasks: ['htmlhint']
+            },
+            css: {
+                files: ['app/resources/css/main.css'],
+                tasks: ['csslint']
+            },
+            js: {
+                files: ['app/model/main.js', 'Gruntfile.js'],
+                tasks: ['jshint']
             }
         }
 
@@ -47,3 +87,8 @@ module.exports = function(grunt){
     grunt.registerTask('default', []);
 
 };
+
+// ref
+// http://coding.smashingmagazine.com/2013/10/29/get-up-running-grunt/
+// http://csslint.net/about.html
+// test
