715fe1142d3a3a1a213f5f53cbff98d6d0c111ce
vwolfley <vern.wolfley@gmail.com>
2014-03-20 10:27:53 -0700

    Updates to Grunt File

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,31 +6,31 @@ module.exports = function(grunt){
 
     grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+        pkg: grunt.file.readJSON("package.json"),
 
         htmlhint: {
             build: {
                 options: {
-                    'tag-pair': true,
+                    "tag-pair": true,
                     // Force tags to have a closing pair
-                    'tagname-lowercase': true,
+                    "tagname-lowercase": true,
                     // Force tags to be lowercase
-                    'attr-lowercase': true,
+                    "attr-lowercase": true,
                     // Force attribute names to be lowercase e.g. <div id="header"> is invalid
-                    'attr-value-double-quotes': true,
+                    "attr-value-double-quotes": true,
                     // Force attributes to have double quotes rather than single
-                    'doctype-first': true,
+                    "doctype-first": true,
                     // Force the DOCTYPE declaration to come first in the document
-                    'spec-char-escape': true,
+                    "spec-char-escape": true,
                     // Force special characters to be escaped
-                    'id-unique': true,
+                    "id-unique": true,
                     // Prevent using the same ID multiple times in a document
-                    // 'head-script-disabled': false,
+                    // "head-script-disabled": false,
                     // Prevent script tags being loaded in the head for performance reasons
-                    'style-disabled': true
+                    "style-disabled": true
                     // Prevent style tags. CSS should be loaded through
                 },
-                src: ['index.html', 'app/views/*html']
+                src: ["index.html", "app/views/*html"]
             }
         },
 
@@ -38,7 +38,7 @@ module.exports = function(grunt){
         // https://github.com/gruntjs/grunt-contrib-csslint
         csslint: {
             // define the files to lint
-            files: ['app/resources/css/main.css'],
+            files: ["app/resources/css/main.css"],
                 strict: {
                     options: {
                         "import": 0,
@@ -54,14 +54,21 @@ module.exports = function(grunt){
         },
 
         jshint: {
-            all: ['app/model/main.js', 'Gruntfile.js'],
+            all: ["app/model/main.js", "Gruntfile.js"],
                 options: {
-                    strict: true,
+                    // strict: true,
                     sub: true,
                     quotmark: "double",
                     trailing: true,
+                    curly: true,
+                    eqeqeq: true,
+                    unused: true,
+                    scripturl: true,
+                    // This option defines globals exposed by the Dojo Toolkit.
                     dojo: true,
+                    // This option defines globals exposed by the jQuery JavaScript library.
                     jquery: true,
+                    // Set force to true to report JSHint errors but not fail the task.
                     force: true,
                     reporter: require("jshint-stylish")
                 }
@@ -70,27 +77,28 @@ module.exports = function(grunt){
 
         watch: {
             html: {
-                files: ['index.html', 'app/views/*html'],
-                tasks: ['htmlhint']
+                files: ["index.html", "app/views/*html"],
+                tasks: ["htmlhint"]
             },
             css: {
-                files: ['app/resources/css/main.css'],
-                tasks: ['csslint']
+                files: ["app/resources/css/main.css"],
+                tasks: ["csslint"]
             },
             js: {
-                files: ['app/model/main.js', 'Gruntfile.js'],
-                tasks: ['jshint']
+                files: ["app/model/main.js", "Gruntfile.js"],
+                tasks: ["jshint"]
             }
         }
 
 
     });
 
-    grunt.registerTask('default', []);
+    grunt.registerTask("default", []);
 
 };
 
 // ref
 // http://coding.smashingmagazine.com/2013/10/29/get-up-running-grunt/
 // http://csslint.net/about.html
-// test test test
+// http://www.jshint.com/docs/options/
+// test test test test test
